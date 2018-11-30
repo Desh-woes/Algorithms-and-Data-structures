9dffd06c5e8c3dbcb1ab82ec0d206b086f4e2bac
Desh-woes <oadesina17@alustudent.com>
2018-11-30 13:14:38 +0200

    Implemented the Hill climbing algorithm on the infinite monkey theorem

---
+++ Iteration and selection control/Hill_climbing_algorithm.py
@@ -0,0 +1,64 @@
+import random
+import string
+
+# Generate list of strings including space
+alpha = list(string.ascii_lowercase)
+alpha.append(' ')
+
+
+# global to contain words generated from the generate_string function
+word_list = []
+goal = 'methinks it is like a weasel'
+
+
+# Function to generate strings 28 characters long.
+def generate_string():
+    # Check is the list has been filled once, if not, fill it
+    if len(word_list) < len(goal):
+        for x in range(0, len(goal)):
+            y = random.randint(0, len(alpha))
+            word_list.append(alpha[y])
+
+    # Loop through the list and keep correct letters
+    else:
+        for x in range(0, len(word_list)):
+            if word_list[x] != goal[x]:
+                y = random.randint(0, len(alpha))
+                word_list[x] = alpha[y]
+
+    # Concatenate all letters in word_list to form a word
+    word = ''
+    for x in word_list:
+        word = word+x
+
+    # Return word that has been created
+    return word
+
+
+# Function to compare generated string to the goal
+def compare_string():
+    word = generate_string()
+    score = 0
+    # Increase score if items are the same with regards to position
+    for items in range(0, len(word)):
+        if word[items] == goal[items]:
+            score += 1
+    return word, score
+
+
+# Function to keep track of strings until the sting is 100% correct
+def best_string():
+    word, score = compare_string()
+    count = 0
+
+    # Call function until the string generated is completely correct.
+    while score < 28:
+        word, score = compare_string()
+        print(word, score)
+        count += 1
+    print(word)
+    print(len(word))
+
+
+best_string()
+
