02caede176e8eef8660c32b2443502dc74bf5233
Desh-woes <oadesina17@alustudent.com>
2019-01-24 21:55:31 +0200

    Character Permutation -- Approach 2

--- Practice/Character_permutation.py
+++ Practice/Character_permutation.py
@@ -1,19 +1,53 @@
 # Approach 1
 # Define test arrays
-arr_1 = ['', '']
-arr_2 = ['', 'a']
+# arr_1 = ['', '']
+# arr_2 = ['', 'a']
+#
+#
+# # Define function to check if the two arrays of characters are permutations of one another
+# def check_arr(arr1, arr2):
+#     if len(arr1) != len(arr2):
+#         return False
+#     else:
+#         arr2 = set(arr2)
+#         for items in arr1:
+#             if items not in arr2:
+#                 return False
+#         return True
+#
+#
+# print(check_arr(arr_1, arr_2))
+
+# Approach 2
+# Define test arrays
+arr_1 = ['r', 'a', 'u', 'd', 'a']
+arr_2 = ['a', 'u', 'd', 'a', 'r']
 
 
-# Define function to check if the two arrays of characters are permutations of one another
+# Define a function to check if array two is a permutation of array 1
 def check_arr(arr1, arr2):
+
+    # A permutation element must have the same size
     if len(arr1) != len(arr2):
         return False
+
+    # sort the list and check if all elements are the same
     else:
-        arr2 = set(arr2)
-        for items in arr1:
-            if items not in arr2:
+        arr1.sort()
+        arr2.sort()
+
+        # Use while loop
+        iterator = 0
+        while iterator < len(arr1):
+            if arr1[iterator] != arr2[iterator]:
                 return False
+            iterator += 1
+
+        # Return true if all values are equal
         return True
 
 
 print(check_arr(arr_1, arr_2))
+
+
+
